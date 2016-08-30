123a87b09ca763aa944a68d04f31fa582148f763
Harrison Valetski <sonnyfromtacoma@msn.com>
2016-08-30 13:56:35 -0400

    Create Chap5_ex9FuelStops_Canada.cs

---
+++ Chap5_ex9FuelStops_Canada.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication22
+{
+    class Canada
+    {
+
+
+
+
+
+
+
+
+    }
+}
