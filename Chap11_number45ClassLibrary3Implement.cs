b680e6c322729c9af4c577a6b298af1a54f560fe
Harrison Valetski <sonnyfromtacoma@msn.com>
2016-08-30 15:17:51 -0400

    Create Chap11_number45ClassLibrary3Implement.cs

---
+++ Chap11_number45ClassLibrary3Implement.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank;
+using loans;
+namespace ClassLibrary3
+{
+    class Implement : BanksForm
+    {
+
+    }
+}
