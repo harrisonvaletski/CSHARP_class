ea0454a44af557f14519560d0e2d43ec7120a9f7
Harrison Valetski <sonnyfromtacoma@msn.com>
2016-08-30 15:18:29 -0400

    Create Chap11_number45ClassLibrary3Portfolio.cs

---
+++ Chap11_number45ClassLibrary3Portfolio.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary3
+{
+    interface portfolio
+    {
+        string getName();
+        string getAcctDetails();
+        string getAcctStatus();
+    }
+}
